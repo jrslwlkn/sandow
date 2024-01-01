3a19bc5b450646c8c44c6ffb19a58b1be93e7c68
Yaroslav <jrslwlkn@gmail.com>
2024-01-01 15:56:46 -0600

    add description of exercises

--- main.js
+++ main.js
@@ -5,105 +5,120 @@ const ex = [
 		reps_min: 50,
 		reps_max: 120,
 		reps_inc: 5,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "Take a dumb-bell in each hand, and come to the position of attention, as described in the opening sentences in the introduction to the exercises. Now, bend the kneess lightly, and turn the inner side of the arms full to the front. In all exercises with the light-weight dumb-bells, the knees must be bent, that the muscles of the leg may feel the strain of the movements of the upper limbs. Tighten the grip of the hands on the dumb-bells, and make tense the muscles of the arms; then alternately flex or bend each arm at the elbow inwards and upwards, till the dumb-bell is in line with the shoulder, back of the hand to the front, shoulders and elbows well drawn down, and the upper arms close to the sides. In lowering the dumb-bells, straighten the arm to its full length, and repeat the alternate movements till the muscles ache. This exercise will develop chiefly the flexor biceps muscle, and the triceps extensor muscle, of the upper arm."
 	},
 	{
 		id: 2,
 		img: ["./img/2-2.png"],
 		reps_min: 25,
 		reps_inc: 2,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "This exercise is the same movement as that in No. 1, except that in the position of attention the backs of the hands and the forearms are to the front, and, when the latter are flexed upwards on the elbows, the knuckles of the hands are close to the shoulders. The alternate motion of bending and extending the arm at the elbow is to be performed rhythmically but vigorously, until the flexor and extensor muscles are made pliant and firm. The action will have a stimulating effect on the respiratory organs and the circulation of the blood."
 	},
 	{
 		id: 3,
 		img: ["./img/3-1.png", "./img/3-2.png"],
 		reps_min: 10,
 		reps_inc: 1,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "Come to the position of attention, knees bent as before, and raise both arms outwards, at full length, in a line with the shoulders. Now, turn the inner side of the forearms upwards, and alternately flex each inwards toward the head, until the dumb-bell is immediately over the shoulder. In practising this movement, maintain the arms rigidly in alignment with the shoulders; in other words, do not let them droop; and, in the straightening-out movement, extend the arms fully, and put the muscles to the strain. The chief muscle that comes into exercise here, besides the biceps and triceps of the arm, is the deltoid, the great muscle that caps the shoulder. The effect of these alternate arm-flexings is perhaps more beneficial than when both arms are flexed at the same time. Its chief advantage is that it gives one arm a momentary alternate rest, and does not overstrain the heart by unduly forcing the circulation of the blood."
 	},
 	{
 		id: 4,
 		img: ["./img/4-1.png", "./img/4-2.png"],
 		reps_min: 10,
 		reps_inc: 0.333,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "This exercise is the same as the last, the flexing movement of the forearms, however, being concurrent or simultaneous, and not alternate. See photo. No. 4, and the caution to be observed, indicated in the closing sentence of the preceding exercise. In the alternate straightening or opening-out movement, care should be taken to extend the arm fully, so that the extensor muscles may have fair play in counteracting the motion of the pulling-up or flexing muscles.<br> The exercise may be varied with advantage by curving the back slightly and bending the head downwards; at the same time bringing the flexed forearms inwards, underneath the upper arms and shoulders, and in this attitude ply the dumb-bells outwards from the armpits to the full extension of each arm. The exercise will be found beneficial for the biceps, triceps, and deltoid. It will also stimulate the breathing and quicken the blood-currents, to many perhaps the chief need as well as the great advantage of active muscular exercise. In the regular alternation of movements, such as are here and elsewhere in the series indicated, the young pupil should try to observe cadence, for a rhythmic movement tends to the automatic performance of the exercises, and so lessens the sense of fatigue, by relieving the brain of care in directing the muscle-action. The habit, however, of thorough work must be first formed, and the mind fixed on this, before allowing the movements to become automatic."
 	},
 	{
 		id: 5,
 		img: ["./img/5-1.png", "./img/5-2a.png", "./img/5-2b.png"],
 		reps_min: 5,
 		reps_inc: 0.5,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "From the attitude of attention, simultaneously raise both anus forwards and full to the front, curving them upwards until the hands and dumb-bells meet together in a line with the mouth, elbows straight, head well back. The dumb-bells in this exercise should be held perpendicularly, not horizontally. From the position attained, simultaneously throw both arms smartly back, well to the rear, and in a line with the shoulders, chest well out. Return them quickly to the front again, and repeat the opening-out movement as often and as vigorously as you can. This exercise is designed to open out the chest, and to loosen and give mobility to the pectoral muscles of the chest, and those in the region of the shoulders. It will be found to have a blood-relieving effect on the organs of the chest and head."
 	},
 	{
 		id: 6,
 		img: ["./img/6-2.png"],
 		reps_min: 15,
 		reps_inc: 0.5,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "From the position of attention, flex both forearms upwards from the elbow, palms inwards, as shown in the left arm of photograph No. 6. Now, alternately raise each arm in a vertical line with the body, taking care to extend the arm over the head to its full length. The return movement should bring the elbow back close to the side and well to the rear. The head and trunk should be kept straight, the chest pressed forwards, and the arms kept well back, during this movement. The muscles brought into play in this exercise, in addition to the biceps, triceps, and deltoid, are those of the back and sides, chiefly the trapesius, latissimus dorsi, and teres major. Their action tends to open the chest and increase its mobility."
 	},
 	{
 		id: 7,
 		img: ["./img/7-1.png", "./img/7-2.png"],
 		reps_min: 10,
 		reps_inc: 0.5,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "Take the position of attention; the hands and dumb-bells resting lightly on the front of the thighs, knuckles outwards, knees bent, chest drawn inwards and downwards, back slightly curved. Raise the arms alternately, stretched to their full extent, forwards and upwards, till they are in a line with the top of the head, lowering the one arm as the other is raised. Maintain this exercise as long and as briskly as possible, taking care to leave the lungs and breathing actio nabsolutely free and unimpeded. The movement tends to increase the mobility of the shoulder-joints, and especially to strengthen the anterior deltoid, the serratus magnus, latissimus dorsi and pectoral muscles."
 	},
 	{
 		id: 8,
 		img: ["./img/8-1.png", "./img/8-2.png"],
 		reps_min: 20,
 		reps_inc: 1,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "This exercise will he found useful for loosening and making flexible the muscles of the wrist. From the position of attention, elevate both arms outwards until they are at right angles with the body, keeping them rigid and the muscles tense. Then, turn each hand and dumb-bell simultaneously round as far as possible on the axis of the wrist, maintaining the movement till pliancy is imparted and the muscles are tired. The exercise may be supplemented by bending the hand backwards and forwards on the wrist. See that the arms do not droop from the shoulder alignment, and that they are not allowed to turn on the elbow-joint: the movement is wholly executed by the wrists."
 	},
 	{
 		id: "9-10",
 		img: ["./img/9-1.png", "./img/9-2.png"],
 		reps_min: 20,
 		reps_inc: 1,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "Exercise 9: Take up the dumb-bells by the sphere or bulb ends, grasping the bosses firmly in the hollow of the hands. Now, simultaneously raise the arms outwards, in a line with the body, till they reach the level of the shoulders. In this position, rotate the right-hand dumb-bell from left to right, and the left-hand dumb-bell from right to left, by a circular motion of the wrist. Keep up the exercise till the muscles tire. The rotary movement is executed wholly by the wrist, and will be found to act beneficially on the numerous muscles of the forearm, and tend to give them mobility.<br> Exercise 10: This is the same movement reversed; that is to say, the rotary movement of the right hand should be from right to left, and of the left hand from left to right. The continued exercise of this movement will give flexibility to the muscles of the forearm, and impart to them strength and endurance."
 	},
 	{
 		id: "11-12",
 		img: ["./img/11-1.png", "./img/11-2a.png", "./img/11-2b.png", "./img/12-2.png"],
 		reps_min: 20,
 		reps_inc: 0.5,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "Exercise 11: Place the dumb-bells on the floor, where they should lie lengthwise along the outer side of each foot, the centre of the bar on a line with the toes. Seize them and rise to the posi- tion of attention, the head and body erect, the knees unbent. Turn half round on the heels to the left, the toes being at an angle of 60 degrees; the body, which should turn on the hips, ought, as much as possible, to keep the front position. At the same time, bring the left forearm upwards to the waist, at right angles with the body; take a good step forward with the right foot, and lunge out forcibly with the left arm in the same direction, as if striking a hard blow, and recover quickly. Bring back the advanced leg with the alternate recover. Repeat the movement until the muscles are well exercised, the right arm remaining tense by the side. In the return movement, bring the elbow well back, and press the chest well forward. The muscles brought in to play in this exercise are the anterior deltoid, the biceps, the triceps, the serratus magnus, and the pectoralis major. When the body is turned on the hip, the lunging movement is beneficial to the abdominal muscles and assists circulation in that region.<br> Exercise 12: This is the same movement reversed; bringing into play the right arm and left foot instead of the left arm and right foot. The half-turn will consequently be to the right, and the left foot be advanced, to maintain the balance of the body. In these movements circulation and respiration are materially benefited. The breath should be inhaled as the arms are drawn back, and exhaled when thrust forward. Thisand the previous exercise, it will be noted, vary from those which precede them, in this respect, that the pulling-up muscles have hitherto been exercised, while the stretching-out ones come now into play."
 	},
 	{
 		id: 13,
 		img: ["./img/13-1.png", "./img/13-2a.png", "./img/13-2b.png"],
 		reps_min: 3,
 		reps_inc: 0.333,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "This exercise is practiced without the dumb-bells. From the position of attention, the pupil will throw himself forward towards the floor, supporting the body, in a rigid position, on the unbent arms and the toes; then, alternately lower the body, by slowly bending the elbows, until it reaches the prone position, and raise it, by straightening the arms, repeating the movement as many times as possible. Care should be taken that the body and lower limbs are kept rigidly straight and do not touch the floor, that the head is kept well up and the knees unbent. The exercise, will be found excellent for strengthening all the muscles of the body, and for expanding the chest. As the strain, in the dipping and raising of the body, is severe, the movement should be indulged in mildly, until the biceps and triceps are pretty well hardened. When facility in themovement has been gained, the effort should be made to stretch the body, in the prone position, horizontally forwards as far as possible (nose more in front), at each performance, that the full benefit of the exercise may be obtained."
 	},
 	{
 		id: 14,
 		img: ["./img/14-2a.png", "./img/14-2b.png"],
 		reps_min: 2,
 		reps_inc: 0.333,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "This exercise is the same as No. 13, only rendered more difficult by the tension of the rubber straps which encircle the neck, and, by the resisting power, increase the development of the arms in the effort to raise the body from the prone position."
 	},
 	{
 		id: 15,
 		img: ["./img/15-1.png"],
 		reps_min: 3,
 		reps_inc: 0.5,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "This exercise is designed to bring into play the rectus abdominis and other muscles of the abdomen, and has an important effect on the digestion. It should at first be performed without the dumb-bells, then with dumb-bells of increasing weight. Lie flat on the back on the floor, couchor bench, covered by a rug, at full length, the arms close by the sides, the feet pushed under the bureau, weighted or strapped to the floor, to give purchase to the body; then alternately raise the body on the hip-joints, from the prone to the sitting position, and slowly lower it again to the horizontal position, repeating the movements until the abdominal muscles feel the tiring effect of the exercise. After a time, when the pupil has accustomed himself to the strain of the movement, he may render it more difficult by taking a dumb-bell in each hand, and, when in the prone position, raising the arms and stretching them back over the head, and then going through with the trunk-raising and lowering movements, as above described. The exercise may also be performed without weighting or strapping the feet. A deep breath should be taken before raising the body, and exhaled in lowering it. In raising the body to the sitting position, it should also be bent forwards as far as possible towards the feet. With the dumb-bells in the hands, it will be found advantageous also to cross the wrists over the head, and so bring the body upwards and forwards, the head locked, as it were, in the upward-extended arms and moved in unison with them. For persons of full habit and having a tendency to be fat, the exercise will be found very beneficial, the increased blood-circulation absorbing the fatty deposits, and the exercise itself being unfavourable to fatty formation."
 	},
 	{
 		id: 16,
 		img: ["./img/16-1.png"],
 		reps_min: 10,
 		reps_inc: 0.5,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "This is a squatting exercise designed to develop the quadriceps extensor, or great extensor muscle of the thigh. Take a dumb-bell in each hand, and come to the position of attention, the body straight, the head erect, the chest thrust out, and the shoulders and hips held well back. By bending the knees, dip the body in a vertical line to the heels, keeping the back straight and the chin drawn in. Recover and repeat the movement until the muscles ache. This is a good exercise in poising the body and in giving suppleness to the knee-joints. If the muscles of the leg and thigh have been well toned, their natural elasticity will render the movement easy. Take care not to let the body sway or incline forwards or backwards on the hips. After a pause, the exercise may be varied by raising the heels and throwing the weight of the body entirely on the toes, keeping rigidly the position of attention, and rising as high as possible in each motion without losing the balance. Continue the movement for some time, as it will be found of much benefit to the muscles of the calf; it will also give elasticity to those of the foot and ankle."
 	},
 	{
 		id: 17,
 		img: ["./img/17-1.png"],
 		reps_min: 15,
 		reps_inc: 0.5,
-		duration_ms: 2000
+		duration_ms: 2000,
+		desc: "This exercise may be practised either with or without dumb-bells. From the position of attention, slowly bend the trunk cutwards on the hip-joint, alternately to the left and right, the hand or dumb-bell slightly pressing the outer side of the thigh, and slipping down until it reaches the bend of the knee. When one hand touches the side of the knee the other hand should be raised under the arm just above the serratus magnus muscle. The exercise will be good for the balancing muscles of the trunk as well as for the obliquus abdominis and other muscles that support and protect the sides of the abdomen. It will also give flexibility to the back-bone, and increase the blood circulation, chiefly along the feeding veins of the stomach and the liver."
 	}
 ];
 
@@ -140,6 +155,10 @@ function navigate(idx) {
 	prev.disabled = idx == 0;
 	next.disabled = idx == ex.length - 1;
 	counter.style.color = paused ? "red" : "green";
+	const description = document.createElement("p");
+	description.innerHTML = ex[idx].desc;
+	description.style = "text-align: justify";
+	images.appendChild(description);
 }
 
 function getReps() {
